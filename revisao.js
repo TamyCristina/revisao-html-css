44bc02c3a8f21176b458c83711ecd6ec10efa88c
TamyCristina <112989809+TamyCristina@users.noreply.github.com>
2022-10-20 15:09:50 -0300

    Arquivos adicionados

---
+++ revisao.js
@@ -0,0 +1,33 @@
+let nomeColaboradora="Maria"
+console.log(nomeColaboradora)
+
+let numero1=20
+let numero2=30
+let obterResultadoSoma=numero1+numero2
+console.log(obterResultadoSoma)
+
+const string="nome"
+const inteiro= 11
+const float=11.3
+const booleanoTrue= true
+const booleanoFalse= false
+console.log(typeof(string))
+
+const nome= "Eduarda"
+// Imprimir apenas a primeira letra
+console.log(nome.charAt)
+console.log(nome[0])
+//para pegar a ultima letra:
+console.log(nome[nome.length -1])
+//Quantidade de letras
+console.log(nome.length)
+
+var x= 40
+var y=30
+if (x>y){
+    console.log("O maior número é x")
+}
+else{
+    console.log("O maior número e y")
+}
+
